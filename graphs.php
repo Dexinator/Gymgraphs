0528101ebc69c9281e0c39a1bc79598a7c6dca2f
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2022-08-17 02:03:07 -0500

    Se entendió el funcionamiento de la gráfica de rango, se obtuvieron los valores de la base de datos y calcularon valores máximos, mínimos y promedios.

--- graphs.php
+++ graphs.php
@@ -1,9 +1,9 @@
 <?php
-/*
+
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$password = "";
+$dbname = "calmecac";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,21 +12,31 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT * FROM records_agosto";
 $result = $conn->query($sql);
 
+$myArray = array();
 if ($result->num_rows > 0) {
-  // output data of each row
+// output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    $myArray[] = $row;
   }
-} else {
+} 
+else 
+{
   echo "0 results";
 }
 $conn->close();
 
-*/
+
 ?>
+
+<!-- HTML -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+
 <!-- Resources -->
 <link rel="stylesheet" href="style.css">
 <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
@@ -34,12 +44,78 @@ $conn->close();
 <script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
 <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
 
+<body>
+  <h1> <img src="logo.jpg" id="imgprinc"></h1>
+  <h2>Promedio Push Press</h2>
+<div class="conten">
+ <div class="split left">
+  <div >
+    <img src="pushpress.gif" alt="Avatar woman" class="imgleft">
+  </div>
+</div>
+
+<div class="split right">
+  <div  id="chartdiv">
+  </div>
+</div> 
+</div>
+
+
+
 <!-- Chart code -->
 <script>
+  var query_result =  <?php echo json_encode($myArray); ?>;
+  console.log(query_result);
+  var total_bench_press = 0;
+  console.log(query_result[1].bench_press);
+
+  query_result.forEach((member) => {
+    console.log(member.bench_press);
+    member.bench_press=Number(member.bench_press);
+    member.push_press=Number(member.push_press);
+    member.back_squat=Number(member.back_squat);
+    total_bench_press+=member.bench_press;
+  });
+  var push_press_array = [];
+  var bench_press_array = [];
+  var back_squat_array = [];
+  for(var i in query_result){
+    push_press_array.push(query_result[i].push_press);
+    bench_press_array.push(query_result[i].bench_press);
+    back_squat_array.push(query_result[i].back_squat);
+
+  }
+
+  var min_push_press=Math.min.apply(null, push_press_array);
+  var min_bench_press=Math.min.apply(null, bench_press_array);
+  var min_back_squat=Math.min.apply(null, back_squat_array);
+
+  var max_push_press=Math.max.apply(null, push_press_array);
+  var max_bench_press=Math.max.apply(null, bench_press_array);
+  var max_back_squat=Math.max.apply(null, back_squat_array);
+
+  var avg_push_press= push_press_array.reduce((a,b) => a + b, 0) / push_press_array.length;
+  var avg_bench_press=push_press_array.reduce((a,b) => a + b, 0) / bench_press_array.length;
+  var avg_back_squat= push_press_array.reduce((a,b) => a + b, 0) / back_squat_array.length;
 
 
+
+  console.log(avg_push_press);
+  console.log(Math.min.apply(null, bench_press_array));
+  console.log(back_squat_array);
+
+
+  console.log(total_bench_press);
+//console.log(query_result[1].start);
+/*
+for (let i = 0; i < query_result.length; i++) {
+  query_result[i].start=new Date(query_result[i].start*1000);
+}
+*/
 
 am5.ready(function() {
+
+
 
 // Create root element
 // https://www.amcharts.com/docs/v5/getting-started/#Root_element
@@ -49,7 +125,7 @@ var root = am5.Root.new("chartdiv");
 // https://www.amcharts.com/docs/v5/concepts/themes/
 root.setThemes([
   am5themes_Animated.new(root)
-]);
+  ]);
 
 // Create chart
 // https://www.amcharts.com/docs/v5/charts/radar-chart/
@@ -58,11 +134,14 @@ var chart = root.container.children.push(
     panX: false,
     panY: false,
     startAngle: 180,
-    endAngle: 360
-  })
-);
+    endAngle: 360,
 
-chart.getNumberFormatter().set("numberFormat", "#'%'");
+  })
+  );
+
+
+
+chart.getNumberFormatter().set("numberFormat", "#");
 
 // Create axis and its renderer
 // https://www.amcharts.com/docs/v5/charts/radar-chart/gauge-charts/#Axes
@@ -76,20 +155,23 @@ axisRenderer.grid.template.setAll({
   strokeOpacity: 0.8
 });
 
+//Valores iniciales
 var xAxis = chart.xAxes.push(
   am5xy.ValueAxis.new(root, {
     maxDeviation: 0,
-    min: 0,
-    max: 100,
+    min: min_push_press,
+    max: max_push_press,
     strictMinMax: true,
     renderer: axisRenderer
   })
-);
+  );
 
 // Add clock hand
 // https://www.amcharts.com/docs/v5/charts/radar-chart/gauge-charts/#Clock_hands
 var axisDataItem = xAxis.makeDataItem({});
 
+
+//Circulo del centro
 var clockHand = am5radar.ClockHand.new(root, {
   pinRadius: 50,
   radius: am5.percent(100),
@@ -117,10 +199,12 @@ var bullet = axisDataItem.set(
   am5xy.AxisBullet.new(root, {
     sprite: clockHand
   })
-);
+  );
 
 xAxis.createAxisRange(axisDataItem);
 
+
+//texto del circulo del centro
 var label = chart.radarContainer.children.push(
   am5.Label.new(root, {
     centerX: am5.percent(50),
@@ -128,12 +212,13 @@ var label = chart.radarContainer.children.push(
     centerY: am5.percent(50),
     fontSize: "1.5em"
   })
-);
+  );
 
-axisDataItem.set("value", 50);
+
+axisDataItem.set("value", min_push_press);
 bullet.get("sprite").on("rotation", function () {
   var value = axisDataItem.get("value");
-  label.set("text", Math.round(value).toString() + "%");
+  label.set("text", Math.round(value).toString());
 });
 
 setInterval(function () {
@@ -141,58 +226,60 @@ setInterval(function () {
 
   axisDataItem.animate({
     key: "value",
-    to: value,
+    to: avg_push_press,
     duration: 500,
     easing: am5.ease.out(am5.ease.cubic)
   });
 
   axisRange0.animate({
     key: "endValue",
-    to: value,
+    to: avg_push_press,
     duration: 500,
     easing: am5.ease.out(am5.ease.cubic)
   });
 
   axisRange1.animate({
     key: "value",
-    to: value,
+    to: avg_push_press,
     duration: 500,
     easing: am5.ease.out(am5.ease.cubic)
   });
-}, 2000);
+});
 
 chart.bulletsContainer.set("mask", undefined);
 
 var colorSet = am5.ColorSet.new(root, {});
-
+//valor inicial y final de la izquierda
 var axisRange0 = xAxis.createAxisRange(
   xAxis.makeDataItem({
     above: true,
-    value: 0,
-    endValue: 50
+    value: min_push_press,
+    endValue: min_push_press
   })
-);
+  );
 
 axisRange0.get("axisFill").setAll({
   visible: true,
-  fill: colorSet.getIndex(0)
+  fill: "black"
 });
 
 axisRange0.get("label").setAll({
   forceHidden: true
 });
 
+//valor inicial y final de la derecha
 var axisRange1 = xAxis.createAxisRange(
   xAxis.makeDataItem({
     above: true,
-    value: 50,
-    endValue: 100
+    value: min_push_press,
+    endValue: max_push_press
   })
-);
+  );
 
+//Color de la derecha
 axisRange1.get("axisFill").setAll({
   visible: true,
-  fill: colorSet.getIndex(4)
+  fill: colorSet.getIndex(10)
 });
 
 axisRange1.get("label").setAll({
@@ -202,8 +289,10 @@ axisRange1.get("label").setAll({
 // Make stuff animate on load
 chart.appear(1000, 100);
 
+
 }); // end am5.ready()
+
 </script>
 
-<!-- HTML -->
-<div id="chartdiv"></div>
+</body>
+</html>
